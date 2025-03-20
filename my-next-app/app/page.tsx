9343c3feaf0b929525393f6d1c13be84dea5620d
Boojana <boojanashree.g@appteq.in>
2025-03-20 11:57:06 +0530

    test 1 push

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changess instantly.
+            Save and see yourdd changess instantly.
           </li>
         </ol>
 
